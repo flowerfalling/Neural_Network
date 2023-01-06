edc6d642907e37366c65ca5a87ef6775eb386c74
falling <zhaowenrui_8866@163.com>
2023-01-06 21:03:44 +0800

    Change

--- cnn_mnist.py
+++ cnn_mnist.py
@@ -6,6 +6,7 @@
 import csv
 import numpy as np
 from abc import ABCMeta, abstractmethod
+import scipy
 
 
 class MNIST:
@@ -35,32 +36,91 @@ class MNIST:
 
 
 class Layer(metaclass=ABCMeta):
-    @staticmethod
-    def relu(x):
-        return np.max(0, x)
+    def __init__(self, lr=0):
+        self.lr = lr
 
     @abstractmethod
     def forward(self, x):
         pass
 
     @abstractmethod
-    def update(self):
+    def update(self, e, h):
         pass
 
 
 class Linear(Layer):
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size, output_size, lr):
+        super().__init__(lr)
         self.weights = np.random.randn(output_size, input_size)
 
     def forward(self, x):
-        return self.relu(np.dot(self.weights, x))
+        return np.dot(self.weights, x)
 
-    def update(self):
+    def update(self, e, h):
+        self.weights += self.lr * np.dot(e, h.T)
+        e = np.dot(e, self.weights.T)
+        return e
+
+    def __call__(self, x):
+        return self.forward(x)
+
+
+class Relu(Layer):
+    def forward(self, x):
+        x[x < 0] = 0
+        return x
+
+    @staticmethod
+    def reforward(x):
+        x[x > 0] = 1
+        x[x < 0] = 0
+        return x
+
+    def update(self, e, h):
+        return e * self.reforward(h)
+
+    def __call__(self, x):
+        return self.forward(x)
+
+
+class Sigmod(Layer):
+    def forward(self, x):
+        return scipy.special.expit(x)
+
+    def update(self, e, o):
+        return e * o * (1 - o)
+
+    def __call__(self, x):
+        self.forward(x)
+
+class Sigmod(Layer):
+    def forward(self, x):
+        pass
+
+    def update(self, e, h):
+        pass
+
+
+class Net(Layer):
+    def __init__(self, lr):
+        super().__init__(lr)
+        self.fc1 = Linear(784, 200, self.lr)
+        self.fc2 = Linear(200, 100, self.lr)
+        self.fc3 = Linear(100, 10, self.lr)
+        self.relu = Relu()
+        self.sigmod = Sigmod()
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.relu(self.fc3(x))
+        pass
+
+    def update(self, e, h):
         pass
 
 
 def main():
-    trainset = MNIST()
     pass
 
 
