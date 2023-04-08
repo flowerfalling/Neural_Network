86b1114a6e6afc9c555960a6f437f9c6c39855ba
falling <zhaowenrui_8866@163.com>
2023-04-08 22:06:05 +0800

    beta init

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/4/8 21:23
+# @Author  : 之落花--falling_flowers
+# @File    : __init__.py
+# @Software: PyCharm
---
+++ src/nn.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/4/8 21:23
+# @Author  : 之落花--falling_flowers
+# @File    : nn.py.py
+# @Software: PyCharm
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
